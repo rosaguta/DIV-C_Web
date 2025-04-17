390a6679008c65d286a88ec33e60678e05f991df
DigitalRose <guta.rob@hotmail.nl>
2025-04-17 15:41:22 +0200

    toast

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import "@/styles/globals.css";
-
+import { Toaster } from "@/components/ui/sonner"
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return<><Component {...pageProps} /><Toaster/></>;
 }
